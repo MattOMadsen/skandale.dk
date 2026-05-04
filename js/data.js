cc4f47429367a41d1fe8c7e18aefe527c187eb11
MattOMadsen <matt@graverholtmurerfirma.dk>
2026-05-04 19:58:16 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-// js/data.js - Data og indlæsning
+// js/data.js - Data og indlæsning (kun 8 politikere - midlertidig fix)
 let politicians = [];
 
 async function loadPoliticians() {
@@ -10,13 +10,13 @@ async function loadPoliticians() {
     'data/politicians/lars-loekke-rasmussen.json',
     'data/politicians/pia-kjaersgaard.json',
     'data/politicians/anders-fogh-rasmussen.json',
-    'data/politicians/morten-messerschmidt.json',
+    'data/politicians/morten-messerschmidt.json'
   ];
 
   try {
     const responses = await Promise.all(politicianFiles.map(file => fetch(file)));
     politicians = await Promise.all(responses.map(res => res.json()));
-    console.log('%c[Skandale.dk] Alle 12 politikere loaded succesfuldt', 'color:#10b981');
+    console.log('%c[Skandale.dk] 8 politikere loaded succesfuldt', 'color:#10b981');
   } catch (error) {
     console.error('Kunne ikke loade data:', error);
   }
